b5ffbf233d5f1c69ac245df89be9b0b8eb905729
kadet <techkadet@gmail.com>
2020-11-06 15:45:09 +0100

    added user validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -66,7 +66,6 @@ const Form = () => {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [field, setField] = useState("");
-
   const [error, setError] = useState(false);
   const [alert, setAlert] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -95,26 +94,57 @@ const Form = () => {
 
       setTimeout(() => {
         const db = firebase.firestore();
+        const registeredUsers = [];
 
-        db.settings({
-          timestampsInSnapshots: true,
-        });
 
-        // eslint-disable-next-line
-        const userRef = db.collection("users").add({
-          name,
-          email,
-          phone: `+${phone}`,
-          field,
-        });
+        db.collection("users")
+          .get()
+          .then(querySnapshot => {
 
-        setName("");
-        setEmail("");
-        setPhone("");
-        setField("");
+            querySnapshot.forEach((doc) => {
+              registeredUsers.push(doc.data().email.toString());
+            });
 
-        setAlert(true);
-        setLoading(false);
+          })
+          .then(() => {
+
+            // console.log(registered)
+
+            if (registeredUsers.includes(email.toString())) {
+              setError({
+                display: true,
+                msg: "This email is already registered",
+              });
+
+              setLoading(false);
+              setTimeout(() => {
+                setError(false)
+              }, 3000);
+
+            } else {
+
+              db.settings({
+                timestampsInSnapshots: true,
+              });
+
+              // eslint-disable-next-line
+              const userRef = db.collection("users").add({
+                name,
+                email,
+                phone: `+${phone}`,
+                field,
+              });
+
+              setName("");
+              setEmail("");
+              setPhone("");
+              setField("");
+
+              setAlert(true);
+              setLoading(false);
+            }
+
+          });
       }, 2000);
     }
   };
@@ -124,33 +154,33 @@ const Form = () => {
       <h3>Join Checkmate Community</h3>
 
       {error.display && (
-        <Error justify='flex-start' align='center' padding='.7rem 1rem'>
-          <span className='material-icons'>error_outline</span>
+        <Error justify="flex-start" align="center" padding=".7rem 1rem">
+          <span className="material-icons">error_outline</span>
           <p> {error.msg} </p>
         </Error>
       )}
 
       <Input
-        type='text'
-        placeholder='Your full name'
+        type="text"
+        placeholder="Your full name"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <Input
-        type='email'
-        placeholder='Your email address'
+        type="email"
+        placeholder="Your email address"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <Input
-        type='number'
-        placeholder='Phone number'
+        type="number"
+        placeholder="Phone number"
         value={phone}
         onChange={(e) => setPhone(e.target.value)}
       />
       <Input
-        as='select'
-        placeholder='Select your skillset'
+        as="select"
+        placeholder="Select your skillset"
         value={field}
         defaultValue={field}
         onChange={(e) => setField(e.target.value)}
@@ -158,22 +188,22 @@ const Form = () => {
         <option value={null} hidden>
           Skill Category
         </option>
-        <option value='Tech Enthusiast'>Tech Enthusiast</option>
-        <option value='Frontend'>Frontend Dev</option>
-        <option value='Backend'>Backend Dev</option>
-        <option value='Mobile'>Mobile Dev</option>
-        <option value='UI/UX Design'>UI/UX Design</option>
-        <option value='Graphics Design'>Graphics Design</option>
-        <option value='Technical Writing'>Technical Writing</option>
-        <option value='Developer Advocate'>Developer Advocate</option>
-        <option value='Sponsor'>Tech Daddy/Mummy (Sponsors & Mentors)</option>
+        <option value="Tech Enthusiast">Tech Enthusiast</option>
+        <option value="Frontend">Frontend Dev</option>
+        <option value="Backend">Backend Dev</option>
+        <option value="Mobile">Mobile Dev</option>
+        <option value="UI/UX Design">UI/UX Design</option>
+        <option value="Graphics Design">Graphics Design</option>
+        <option value="Technical Writing">Technical Writing</option>
+        <option value="Developer Advocate">Developer Advocate</option>
+        <option value="Sponsor">Tech Daddy/Mummy (Sponsors & Mentors)</option>
       </Input>
-      <Submit type='submit' full>
+      <Submit type="submit" full>
         {loading ? "Please wait..." : "Sign Up"}
       </Submit>
 
       {alert && <ALert alert={setAlert} completed={setCompleted} />}
-      {completed && <Redirect to='/' />}
+      {completed && <Redirect to="/" />}
     </StyledForm>
   );
 };
